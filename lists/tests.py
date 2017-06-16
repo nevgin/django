b0164f6e9ebacd87b5a83ce541afac4e7d3859c8
nevgin <nevgin@yandex.ru>
2017-06-16 17:55:24 +0300

    To do Home

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
+
+# Create your tests here.
 
 class HomePageTest(TestCase):
 
@@ -9,4 +13,14 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-# Create your tests here.
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
+
+
+
